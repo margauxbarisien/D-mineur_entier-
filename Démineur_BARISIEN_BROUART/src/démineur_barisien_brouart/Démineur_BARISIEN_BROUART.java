68d60d3bb05d44b6b2e7e6b77920849391a93d0c
marga <margaux.barisien@epfedu.fr>
2023-01-02 19:10:44 +0100

    découvrir bombes et verif victoire commencées

--- Démineur_BARISIEN_BROUART/src/démineur_barisien_brouart/Démineur_BARISIEN_BROUART.java
+++ Démineur_BARISIEN_BROUART/src/démineur_barisien_brouart/Démineur_BARISIEN_BROUART.java
@@ -30,7 +30,7 @@ public class Démineur_BARISIEN_BROUART extends JFrame implements ActionListener
     int nbKits = 1;
     GridLayout layout = new GridLayout(lignes, colonnes);//création grille en interface
     boolean[] mines = new boolean[lignes * colonnes];
-    boolean[] kits = new boolean[lignes* colonnes];
+    boolean[] kits = new boolean[lignes * colonnes];
     boolean[] caseNonCliquée = new boolean[lignes * colonnes];
     boolean perdu = false;
     boolean gagné = false;
@@ -45,7 +45,7 @@ public class Démineur_BARISIEN_BROUART extends JFrame implements ActionListener
 
     public Démineur_BARISIEN_BROUART() {
         p.setLayout(layout);
-        setupI();
+        sethautI();
         for (int i = 0; i < (lignes * colonnes); i++) {
             p.add(boutons[i]);//ajout des boutons 
         }
@@ -169,7 +169,7 @@ public class Démineur_BARISIEN_BROUART extends JFrame implements ActionListener
         }
         placerMines();//les mines se placent sur la grille
         placerNombres();//les nbr aussi
-        //placerKits();
+        placerKits();
     }
 
     public void setupI2() {
@@ -190,7 +190,7 @@ public class Démineur_BARISIEN_BROUART extends JFrame implements ActionListener
         this.pack();
         placerMines();
         placerNombres();
-        //placerKits();
+        placerKits();
     }
 
     public void setup() {
@@ -205,15 +205,16 @@ public class Démineur_BARISIEN_BROUART extends JFrame implements ActionListener
         }
         placerMines();
         placerNombres();
-        //placerKits();
+        placerKits();
         perdu = false;
         mineLabel.setText("mines: " + nbMines + " marquées: 0");
     }
-public static void main(String[] args) {
-        new Démineur_BARISIEN_BROUART();
+
+    public static void main(String[] args) {
+        new Démineur_BARISIEN_BROUART();//création partie 
     }
 
-public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {
         if (e.getSource() == menuOptions) {
             lignes = Integer.parseInt((String) JOptionPane.showInputDialog(
                     this, "Lignes", "Lignes", JOptionPane.PLAIN_MESSAGE, null,
@@ -226,54 +227,165 @@ public void actionPerformed(ActionEvent e) {
             nbKits = Integer.parseInt((String) JOptionPane.showInputDialog(
                     this, "Kits de déminage", "Kits de déminage", JOptionPane.PLAIN_MESSAGE, null, null, 1));
             setupI2();
-        }
+        }//fenetre de menu avec choix des options
         if (!gagné) {
             for (int x = 0; x < lignes; x++) {
                 for (int y = 0; y < colonnes; y++) {
-                    if (e.getSource() == buttons[(lignes * y) + x]
-                            && !won && clickable[(lignes * y) + x]) {
-                        doCheck(x, y);
+                    if (e.getSource() == boutons[(lignes * y) + x]
+                            && !gagné && caseNonCliquée[(lignes * y) + x]) {
+                        verifVictoire(x, y);
                         break;
                     }
                 }
             }
         }
-        if (e.getSource() == newGameButton) {
+        if (e.getSource() == nouvellePartie) {
             setup();
-            won = false;
+            gagné = false;
             return;
- 
+
         }
-        checkWin();
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        verifWin();
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
     public void mouseEntered(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+    //permet à l'utilisateur de savoir quelle case il survole
 
-    @Override
     public void mouseExited(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    public void mousePressed(MouseEvent e) {
+        if (e.getButton() == 3) {
+            int n = 0;
+            for (int x = 0; x < lignes; x++) {
+                for (int y = 0; y < colonnes; y++) {
+                    if (e.getSource() == boutons[(lignes * y) + x]) {
+                        caseNonCliquée[(lignes * y) + x] = !caseNonCliquée[(lignes * y)
+                                + x];
+                    }
+                    if (!caseCliquée[(lignes * y) + x]) {
+                        if (!caseNonCliquée[(lignes * y) + x]) {
+                            boutons[(lignes * y) + x].setText("X");
+                            n++;//si clique droit on place un "X" on le joueur pense qu'il y a une mine
+                        } else {
+                            boutons[(lignes * y) + x].setText("");
+                        }
+                        mineLabel.setText("mines: " + nbMines + " marquées: "
+                                + n);
+                    }
+                }
+            }
+        }
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void verifVictoire(int x, int y) {
+        int cases = (lignes * y) + x;
+        boolean g = (x - 1) >= 0;
+        boolean d = (x + 1) < lignes;
+        boolean h = (y - 1) >= 0;
+        boolean b = (y + 1) < colonnes;
+        int gauche = (lignes * (y)) + (x - 1);
+        int droite = (lignes * (y)) + (x + 1);
+        int haut = (lignes * (y - 1)) + (x);
+        int hautgauche = (lignes * (y - 1)) + (x - 1);
+        int hautdroite = (lignes * (y - 1)) + (x + 1);
+        int bas = (lignes * (y + 1)) + (x);
+        int basgauche = (lignes * (y + 1)) + (x - 1);
+        int basdroite = (lignes * (y + 1)) + (x + 1);//verif si il y a une bombe 
+
+        caseCliquée[cases] = true;
+        boutons[cases].setEnabled(false);
+        if (nombres[cases] == 0 && !mines[cases] && !perdu && !gagné) {
+            if (h && !gagné) {
+                if (!caseCliquée[haut] && !mines[haut]) {
+                    caseCliquée[haut] = true;
+                    boutons[haut].doClick();//découvre la case quand il n'y a pas de mine en haut
+                }
+                if (g && !gagné) {
+                    if (!caseCliquée[hautgauche] && nombres[hautgauche] != 0
+                            && !mines[hautgauche]) {
+                        caseCliquée[hautgauche] = true;
+                        boutons[hautgauche].doClick();//découvre la case quand il n'y a pas de mine à gauche
+                    }
+                }
+                if (d && !gagné) {
+                    if (!caseCliquée[hautdroite] && nombres[hautdroite] != 0
+                            && !mines[hautdroite]) {
+                        caseCliquée[hautdroite] = true;
+                        boutons[hautdroite].doClick();//découvre la case quand il n'y a pas de mine à droite
+                    }
+                }
+                if (b && !gagné) {
+                    if (!caseCliquée[bas] && !mines[bas]) {
+                        caseCliquée[bas] = true;
+                        boutons[bas].doClick();//découvre la case quand il n'y a pas de mine en bas
+                    }
+                    if (g && !gagné) {
+                        if (!caseCliquée[basgauche] && nombres[basgauche] != 0
+                                && !mines[basgauche]) {
+                            caseCliquée[basgauche] = true;
+                            boutons[basgauche].doClick();//découvre une case adjacente à une ou plusieurs bombes
+                        }
+                    }
+                    if (d && !gagné) {
+                        if (!caseCliquée[basdroite]
+                                && nombres[basdroite] != 0
+                                && !mines[basdroite]) {
+                            caseCliquée[basdroite] = true;
+                            boutons[basdroite].doClick();//découvre une case adjacente à une ou plusieurs bombes
+                        }
+                    }
+                }
+                if (g && !gagné) {
+                    if (!caseCliquée[gauche] && !mines[gauche]) {
+                        caseCliquée[gauche] = true;
+                        boutons[gauche].doClick();//découvre une case adjacente à une ou plusieurs bombes
+                    }
+                }
+                if (d && !gagné) {
+                    if (!caseCliquée[droite] && !mines[droite]) {
+                        caseCliquée[droite] = true;
+                        boutons[droite].doClick();//découvre une case adjacente à une ou plusieurs bombes
+                    }
+                } else {
+                    boutons[cases].setText("" + nombres[cases]);
+                    if (!mines[cases] && nombres[cases] == 0) {
+                        boutons[cases].setText("");//si case vide alors rien ne s'écrit dessus 
+                    }
+                }
+                if (mines[cases] && !gagné) {
+                    boutons[cases].setText("0");
+                    vies--;//si bombe sur case alors on perd une vie et on écrit 0
+                    if (vies == 0) {
+                        doLose();//lorsque les 3 vies perdues la partie est finie
+                    }
+                }
+            }
+
+        }
+    }
+
+    public void verifWin() {
+        for (int x = 0; x < lignes; x++) {
+            for (int y = 0; y < colonnes; y++) {
+                int cases = (lignes * y) + x;
+                if (!caseCliquée[cases]) {
+                    if (mines[cases]) {
+                        continue;
+                    } else {
+                        return;
+                    }
+                }
+            }
+        }
+
+        doWin();
+    }
 }
